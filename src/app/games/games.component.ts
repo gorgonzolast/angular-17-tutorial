291bbd1611dfb8d322f5261409efda55db5ee2cc
davidvaldh <ds.valderrama@uniandes.edu.co>
2024-01-06 00:05:44 -0500

    Feat: Games comp (for directive demo)

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  template: `
+    <ul>
+      @for (game of games; track game.id) {
+        <li>{{ game.name }}</li>
+      }
+    </ul>
+  `,
+  styles: ``
+})
+export class GamesComponent {
+
+  games = [
+    { id: 1, name: 'Super Mario Bros' },
+    { id: 2, name: 'The Legend of Zelda' },
+    { id: 3, name: 'Metroid' }
+  ]
+}
